f6c7b66cdbcebfb44e8a3b79832e8e88df19e70b
dvlopjs <crissenn99@gmail.com>
2024-05-24 12:20:51 -0300

    Sitemap added

---
+++ app/sitemap.ts
@@ -0,0 +1,32 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://dogmamuebles.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://dogmamuebles.vercel.app/cocinas",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://dogmamuebles.vercel.app/placards",
+
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+
+    {
+      url: "https://dogmamuebles.vercel.app/otros",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
+    },
+  ];
+}
